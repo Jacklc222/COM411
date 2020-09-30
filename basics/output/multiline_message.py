0ff2aba8e97087b72467293d41b547b363bd2434
repl.it user <replituser@example.com>
2020-09-30 09:51:54 +0000

    added more code

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,10 @@
+
+print("System Failiure Imminent")
+print("System reboot initiated...")
+
+
+print("... rebooting sensory system")
+print("... rebooting output motors")
+print ("... rebooting hoover engine")
+
+print("System online.")
