2954c6914cba867cfd041271e93d56cbe56f6902
repl.it user <replituser@example.com>
2020-09-30 10:22:47 +0000

    display escape characters

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,10 @@
+#Display escape characters 
+print("\n displays a new line")
+print("\t displays a new tab space")
+print("\\ Displays a backslash")
+print("\" displays a double quote")
+print("\''disaplays a single quote")
+
+
+print()
+print("\t I am Beep!")
